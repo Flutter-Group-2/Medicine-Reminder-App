052822549b4aedbe914b95df47237697cf6ec6ba
Saadxf <vip.saadxf@gmail.com>
2024-03-19 21:36:35 +0300

    setup locator

--- lib/data_layer/data_layer.dart
+++ lib/data_layer/data_layer.dart
@@ -1,3 +1,10 @@
-class HomeData {
-  
+import 'package:get_it/get_it.dart';
+import 'package:medicine_reminder_app/service/supabase_services.dart';
+
+class DataInjection {
+  final locator = GetIt.I;
+
+  setup() {
+    locator.registerSingleton<DBService>(DBService());
+  }
 }

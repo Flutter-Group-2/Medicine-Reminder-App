cc6f249bb050ac5d8fb99df16b3b783fad4c769a
Saadxf <vip.saadxf@gmail.com>
2024-03-19 21:47:02 +0300

    Models add it

--- lib/model/medicine_model.dart
+++ lib/model/medicine_model.dart
@@ -1,1 +1,27 @@
-class MedicineModel {}
+class MedicineModel {
+  String? name;
+  String? time;
+  int? count;
+  int? period;
+  String? userId;
+
+  MedicineModel({this.name, this.time, this.count, this.period, this.userId});
+
+  MedicineModel.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    time = json['time'];
+    count = json['count'];
+    period = json['period'];
+    userId = json['user_id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['time'] = this.time;
+    data['count'] = this.count;
+    data['period'] = this.period;
+    data['user_id'] = this.userId;
+    return data;
+  }
+}
